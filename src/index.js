34b2bff4edbadbf4a17f0476ffe6914d322ff1df
Jonas Finborud Nyman <jonas.nyman@no.experis.com>
2025-02-04 13:38:14 +0100

    Extension completed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 const pokemonData = data;
-const cardList = document.getElementsByClassName("cards")
+const cardList = document.querySelector("#cards")
 
 function generateCards() {
     cardList.innerHTML = ""
@@ -27,9 +27,25 @@ function generateCards() {
             statsList.appendChild(statitem)
         })
 
+        const appearanceTitle = document.createElement("h2")
+        appearanceTitle.textContent = "Game appearances:"
+
+        const appearanceList = document.createElement("ul")
+
+        pokemon.game_indices.forEach(appearance => {
+            const appearanceItem = document.createElement("li")
+            appearanceItem.textContent = `${appearance.version.name}`
+            appearanceList.appendChild(appearanceItem)
+        })
+
+        const gameAppeared = document.createElement("h3")
+        gameAppeared.textContent = `First game appearance: ${pokemon.game_indices[0].version.name}`
+
         item.appendChild(title)
         item.appendChild(img)
         item.appendChild(statsList)
+        item.appendChild(appearanceTitle)
+        item.appendChild(appearanceList)
         
         return item
     })
@@ -37,7 +53,7 @@ function generateCards() {
 
 function main() {
     generateCards().forEach(card => {
-        cardList[0].appendChild(card)
+        cardList.appendChild(card)
     })
 }
 
